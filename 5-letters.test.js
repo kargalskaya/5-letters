62d081f19e472192788b1208142d12d4d6f39320
KARGALSKAYA Olesya <Olesya.A.KARGALSKAYA@raiffeisen.ru>
2022-12-25 12:21:16 +0300

    FiveLetters class methods and tests

--- 5-letters.test.js
+++ 5-letters.test.js
@@ -2,8 +2,151 @@ import { FiveLetters } from "./5-letters";
 
 const fiveLetters = new FiveLetters('буква');
 
-describe('FiveLetters should', () => {
-  test('return "буква"', () => {
+describe('FiveLetters getters should', () => {
+  test('getWord return "буква"', () => {
     expect(fiveLetters.getWord()).toBe('буква');
+  });
+
+  test('getAimSymbols return [\'б\', \'у\', \'к\', \'в\', \'а\']', () => {
+    expect(fiveLetters.getAimSymbols()).toEqual(['б', 'у', 'к', 'в', 'а']);
+  });
+
+  test('getInsertedSymbol from position 0', () => {
+    expect(fiveLetters.getInsertedSymbol(0)).toEqual(undefined);
+  });
+
+  test('getInsertedSymbols from position', () => {
+    expect(fiveLetters.getInsertedSymbols()).toEqual([]);
+  });
+
+  test('setInsertedSymbol symbols after add "б" on first position', () => {
+    fiveLetters.setInsertedSymbol('б');
+    expect(fiveLetters.getInsertedSymbols()).toEqual([{
+      symbol: 'б',
+      exist: true,
+      onPlace: true,
+    }]);
+  });
+
+  test('setInsertedSymbol symbols after add "б" on second position', () => {
+    fiveLetters.setInsertedSymbol('б');
+    expect(fiveLetters.getInsertedSymbols()).toEqual([
+      {
+        symbol: 'б',
+        exist: true,
+        onPlace: true,
+      },
+      {
+        symbol: 'б',
+        exist: true,
+        onPlace: false,
+      },
+    ]);
+  });
+
+  test('setInsertedSymbol symbols after add "о" on third position', () => {
+    fiveLetters.setInsertedSymbol('о');
+    expect(fiveLetters.getInsertedSymbols()).toEqual([
+      {
+        symbol: 'б',
+        exist: true,
+        onPlace: true,
+      },
+      {
+        symbol: 'б',
+        exist: true,
+        onPlace: false,
+      },
+      {
+        symbol: 'о',
+        exist: false,
+        onPlace: false,
+      },
+    ]);
+  });
+
+  test('setInsertedSymbol symbols after adding 6 symbols on max 5', () => {
+    fiveLetters.setInsertedSymbol('о');
+    fiveLetters.setInsertedSymbol('о');
+    fiveLetters.setInsertedSymbol('о');
+    expect(fiveLetters.getInsertedSymbols()).toEqual([
+      {
+        symbol: 'б',
+        exist: true,
+        onPlace: true,
+      },
+      {
+        symbol: 'б',
+        exist: true,
+        onPlace: false,
+      },
+      {
+        symbol: 'о',
+        exist: false,
+        onPlace: false,
+      },
+      {
+        symbol: 'о',
+        exist: false,
+        onPlace: false,
+      },
+      {
+        symbol: 'о',
+        exist: false,
+        onPlace: false,
+      },
+    ]);
+  });
+
+  test('checkResult returns false, "буква" !== "буооо"', () => {
+    expect(fiveLetters.checkResult()).toBe(false);
+  });
+
+  test('removeSymbol remove 3 symbols', () => {
+    fiveLetters.removeSymbol();
+    fiveLetters.removeSymbol();
+    expect(fiveLetters.getInsertedSymbols()).toEqual([
+      {
+        symbol: 'б',
+        exist: true,
+        onPlace: true,
+      },
+      {
+        symbol: 'б',
+        exist: true,
+        onPlace: false,
+      },
+      {
+        symbol: 'о',
+        exist: false,
+        onPlace: false,
+      },
+    ]);
+  });
+
+  test('clearInsertedSymbols clear', () => {
+    fiveLetters.clearInsertedSymbols();
+    expect(fiveLetters.getInsertedWord()).toBe('');
+  });
+
+  test('checkResult returns true, "буква" === "буква"', () => {
+    fiveLetters.setInsertedSymbol('б');
+    fiveLetters.setInsertedSymbol('у');
+    fiveLetters.setInsertedSymbol('к');
+    fiveLetters.setInsertedSymbol('в');
+    fiveLetters.setInsertedSymbol('а');
+    expect(fiveLetters.getInsertedWord()).toBe("буква");
+    expect(fiveLetters.checkResult()).toBe(true);
+  });
+
+  test('checkResult returns false, "слово" === "буква"', () => {
+    fiveLetters.clearInsertedSymbols();
+    fiveLetters.setInsertedSymbol('с');
+    fiveLetters.setInsertedSymbol('л');
+    fiveLetters.setInsertedSymbol('о');
+    fiveLetters.setInsertedSymbol('в');
+    fiveLetters.setInsertedSymbol('о');
+    expect(fiveLetters.getInsertedWord()).toBe("слово");
+    expect(fiveLetters.checkResult()).toBe(false);
   });
 });
--- 5-letters.ts
+++ 5-letters.ts
@@ -1,13 +1,71 @@
+type TInsertedSymbol = {
+  symbol: string,
+  exist: boolean,
+  onPlace: boolean,
+};
+
 export class FiveLetters {
   readonly aimWord: string;
-  readonly aimSymbols: string[];
+  readonly aimSymbols: Map<string, number>;
+  readonly lenght: number;
 
-  constructor(aimWord = 'слово') {
+  tries: number;
+  insertedSymbols: TInsertedSymbol[];
+
+  constructor(aimWord = 'слово', lenght = 5) {
     this.aimWord = aimWord;
-    this.aimSymbols = [...aimWord];
+    this.aimSymbols = new Map();
+    this.lenght = lenght;
+    this.insertedSymbols = [];
+    this.tries = 0;
+    [...aimWord].forEach((item: string) => {
+      const repetition: number = this.aimSymbols.get(item) || 0
+      this.aimSymbols.set(item, repetition + 1);
+    })
   }
 
-  getWord() {
+  getWord(): string {
     return this.aimWord
   };
+
+  getAimSymbols(): string[] {
+    return [...this.aimSymbols.keys()];
+  };
+
+  getInsertedSymbol(position: number): TInsertedSymbol | undefined {
+    return this.insertedSymbols[position];
+  }
+
+  getInsertedSymbols(): TInsertedSymbol[] {
+    return this.insertedSymbols;
+  }
+  getInsertedWord(): string {
+    return this.insertedSymbols.map((el: TInsertedSymbol) => el.symbol).join('');
+  }
+
+  setInsertedSymbol(symbol: string): void {
+    const position = this.insertedSymbols.length;
+    if (this.insertedSymbols.length === this.lenght) return;
+
+    this.insertedSymbols.push({
+      symbol: symbol,
+      exist: Boolean(this.aimSymbols.get(symbol)),
+      onPlace: this.aimWord[position] === symbol,
+    });
+    return;
+  }
+
+  checkResult(): boolean {
+    return this.aimWord === this.getInsertedWord();
+  }
+
+  removeSymbol(): void {
+    this.insertedSymbols = this.insertedSymbols.slice(0, this.insertedSymbols.length - 1);
+    return;
+  }
+
+  clearInsertedSymbols(): void {
+    this.insertedSymbols = [];
+    return;
+  }
 }
